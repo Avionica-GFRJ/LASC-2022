4633b174b976a4740163dd731f885d1f382567d9
gabriel-maximus <gabrielmaximus80@gmail.com>
2022-07-25 22:51:47 -0300

    Primeiro codigo para o ATOM - TTGO

--- Códigos Teste/GPS TTGO GIL/src/main.cpp
+++ Códigos Teste/GPS TTGO GIL/src/main.cpp
@@ -8,7 +8,7 @@
    It requires the use of SoftwareSerial, and assumes that you have a
    4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
 */
-static const int RXPin = 19, TXPin = 23;
+static const int RXPin = 4, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
 
 static void smartDelay(unsigned long);
