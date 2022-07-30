7f920dfa5c56563f12847b707907e18ac6e16070
ljcarneiro <ljunqueiracarneiro@gmail.com>
2022-07-30 19:03:24 -0300

    Codigo do GY80 e GPS

--- ATOM/ATOM TTGO/src/main.cpp
+++ ATOM/ATOM TTGO/src/main.cpp
@@ -10,8 +10,11 @@
 // specific I2C addresses may be passed as a parameter here
 // (though the BMP085 supports only one address)
 
-#include <TinyGPS++.h>
-#include <HardwareSerial.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+
+SoftwareSerial serial1(10, 11); // RX, TX
+TinyGPS gps1;
 
 // Bibliotecas para o GPS
 
@@ -24,18 +27,6 @@ int32_t altitude;
 #define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
 bool blinkState = false;
 
-static const int RXPin = 4, TXPin = 3; //
-static const uint32_t GPSBaud = 9600;
-
-static void smartDelay(unsigned long);
-
-
-// The TinyGPS++ object
-TinyGPSPlus gps;
-
-// The serial connection to the GPS device
-HardwareSerial ss(2);
-
 void setup() {
     // join I2C bus (I2Cdev library doesn't do this automatically)
     Wire.begin();
@@ -43,7 +34,7 @@ void setup() {
     // initialize serial communication
     // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
     // it's really up to you depending on your project)
-    Serial.begin(115200);
+    Serial.begin(9600);
 
     // initialize device
     Serial.println("Initializing I2C devices...");
@@ -56,7 +47,7 @@ void setup() {
     // configure LED pin for activity indication
     pinMode(LED_PIN, OUTPUT);
 
-    ss.begin(GPSBaud,SERIAL_8N1,RXPin,TXPin,false,20000UL);
+    serial1.begin(9600);
 }
 
 void loop() {
@@ -83,13 +74,116 @@ void loop() {
     Serial.print(pressure); Serial.print("\t");
     Serial.print(altitude);
     Serial.println("");*/
+    bool recebido = false;
 
-    float lat = gps.location.lat();
-    float log = gps.location.lng();
+    while (serial1.available()) {
+        char cIn = serial1.read();
+        recebido = gps1.encode(cIn);
+    }
 
-    char stemp [120];
-    sprintf(stemp, "* %10.2f ; %10.2f ;%10.2f ;%10.2f ;%10.2f *",temperature, pressure, altitude, lat ,log);
-    Serial.println(stemp);
+    if (recebido) {
+        Serial.println("----------------------------------------");
+        
+        //Latitude e Longitude
+        long latitude, longitude;
+        unsigned long idadeInfo;
+        gps1.get_position(&latitude, &longitude, &idadeInfo);     
+
+        if (latitude != TinyGPS::GPS_INVALID_F_ANGLE) {
+            Serial.print("Latitude: ");
+            Serial.println(float(latitude) / 100000, 6);
+        }
+
+        if (longitude != TinyGPS::GPS_INVALID_F_ANGLE) {
+            Serial.print("Longitude: ");
+            Serial.println(float(longitude) / 100000, 6);
+        }
+
+        if (idadeInfo != TinyGPS::GPS_INVALID_AGE) {
+            Serial.print("Idade da Informacao (ms): ");
+            Serial.println(idadeInfo);
+        }
+
+
+        //Dia e Hora
+        int ano;
+        byte mes, dia, hora, minuto, segundo, centesimo;
+        gps1.crack_datetime(&ano, &mes, &dia, &hora, &minuto, &segundo, &centesimo, &idadeInfo);
+
+        Serial.print("Data (GMT): ");
+        Serial.print(dia);
+        Serial.print("/");
+        Serial.print(mes);
+        Serial.print("/");
+        Serial.println(ano);
+
+        Serial.print("Horario (GMT): ");
+        Serial.print(hora);
+        Serial.print(":");
+        Serial.print(minuto);
+        Serial.print(":");
+        Serial.print(segundo);
+        Serial.print(":");
+        Serial.println(centesimo);
+
+
+        //altitude
+        float altitudeGPS;
+        altitudeGPS = gps1.f_altitude();
+
+        if ((altitudeGPS != TinyGPS::GPS_INVALID_ALTITUDE) && (altitudeGPS != 1000000)) {
+            Serial.print("Altitude (cm): ");
+            Serial.println(altitudeGPS);
+        }
+
+
+        //velocidade
+        float velocidade;
+        //velocidade = gps1.speed();        //nós
+        velocidade = gps1.f_speed_kmph();   //km/h
+        //velocidade = gps1.f_speed_mph();  //milha/h
+        //velocidade = gps1.f_speed_mps();  //milha/segundo
+
+        Serial.print("Velocidade (km/h): ");
+        Serial.println(velocidade, 2);  //Conversão de Nós para Km/h
+
+
+
+        //sentito (em centesima de graus)
+        unsigned long sentido;
+        sentido = gps1.course();
+
+        Serial.print("Sentido (grau): ");
+        Serial.println(float(sentido) / 100, 2);
+
+
+        //satelites e precisão
+        unsigned short satelites;
+        unsigned long precisao;
+        satelites = gps1.satellites();
+        precisao =  gps1.hdop();
+
+        if (satelites != TinyGPS::GPS_INVALID_SATELLITES) {
+            Serial.print("Satelites: ");
+            Serial.println(satelites);
+        }
+
+        if (precisao != TinyGPS::GPS_INVALID_HDOP) {
+            Serial.print("Precisao (centesimos de segundo): ");
+            Serial.println(precisao);
+        }
+
+
+        //float distancia_entre;
+        //distancia_entre = gps1.distance_between(lat1, long1, lat2, long2);
+
+        //float sentido_para;
+        //sentido_para = gps1.course_to(lat1, long1, lat2, long2);
+        char stemp [120];
+        sprintf(stemp, "* %10.2f ; %10.2f ;%10.2f ;%10.2f ;%10.2f *",temperature, pressure, altitude, latitude ,longitude);
+        Serial.println(stemp);
+    }
+
 
     // blink LED to indicate activity
     blinkState = !blinkState;
